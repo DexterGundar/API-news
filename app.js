5ffaa84af119cb7dae4e0ba86c18ea433cdfb3af
Gundars Geida <gundar@Gundarss-MacBook-Pro.local>
2023-10-02 15:57:41 +0100

    2-Get-api-topics completed

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const { getTopics } = require("./db/controllers/app.controllers.js");
+
+app.get("/api/topics", getTopics);
+
+app.use((err, req, res, next) => {
+        res.status(500).send({ msg: "internal server error!" })
+  })
+
+module.exports = app;
---
+++ db/models/app.models.js
@@ -0,0 +1,9 @@
+
+const db = require("../connection.js");
+
+exports.fetchTopics = () => {
+    return db.query('SELECT * FROM topics;').then(({ rows }) => {
+        return rows;
+    })
+}
+
